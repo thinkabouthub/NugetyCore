3b534e17f4767266e24761941b07c97e4cba09b9
Clare Moore <clare.moore@thinkabout.com.au>
2016-08-31 12:00:35 +1000

    Fix build because test folder has been moved

--- build.bat
+++ build.bat
@@ -25,7 +25,7 @@ REM call dotnet build --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
-cd NugetyCore\src\NugetyCore.Tests
+cd NugetyCore\src\Tests\NugetyCore.Tests
 call dotnet test  --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
