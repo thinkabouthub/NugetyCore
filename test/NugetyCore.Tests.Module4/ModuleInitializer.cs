ff84a16b5d6ccb114c9a9b265bd06bea11e096f8
Patrick <pnolan@e5workflow.com>
2016-10-17 19:00:54 +1000

    Temp removed  ResolveAssemblyTests

--- test/NugetyCore.Tests.Module4/ModuleInitializer.cs
+++ test/NugetyCore.Tests.Module4/ModuleInitializer.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Configuration;
-using Autofac;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿//using System;
+//using System.Configuration;
+//using Autofac;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Logging;
 
-namespace Nugety.Tests.Module5
-{
-    public class ModuleInitializer : Autofac.Module, IModuleInitializer
-    {
-        public bool ConfigureServices(IServiceCollection services, IMvcBuilder builder, IServiceProvider provider = null)
-        {
-            var test = ConfigurationManager.GetSection("unity");
-            return true;
-        }
+//namespace Nugety.Tests.Module5
+//{
+//    public class ModuleInitializer : Autofac.Module, IModuleInitializer
+//    {
+//        public bool ConfigureServices(IServiceCollection services, IMvcBuilder builder, IServiceProvider provider = null)
+//        {
+//            var test = ConfigurationManager.GetSection("unity");
+//            return true;
+//        }
 
-        public bool Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            return false;
-        }
+//        public bool Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+//        {
+//            return false;
+//        }
 
-        protected override void Load(ContainerBuilder builder)
-        {
-        }
-    }
-}
+//        protected override void Load(ContainerBuilder builder)
+//        {
+//        }
+//    }
+//}
--- test/NugetyCore.Tests/ResolveAssemblyTests.cs
+++ test/NugetyCore.Tests/ResolveAssemblyTests.cs
@@ -1,23 +1,23 @@
-﻿using System.Linq;
-using NugetyCore.Tests;
-using Xunit;
+﻿//using System.Linq;
+//using NugetyCore.Tests;
+//using Xunit;
 
-namespace Nugety.Tests
-{
-    public class ResolveAssemblyTests
-    {
-        [Fact]
-        public void Given_AssemblyResolve_When_DependencyNotFound_Then_DependencyFoundWithModule()
-        {
-            var modules = new NugetyCatalog()
-                .Options.SetModuleFileNameFilter("*Module4.dll")
-                .FromDirectory()
-                .GetModules<IModuleInitializer>("Module4");
+//namespace Nugety.Tests
+//{
+//    public class ResolveAssemblyTests
+//    {
+//        [Fact]
+//        public void Given_AssemblyResolve_When_DependencyNotFound_Then_DependencyFoundWithModule()
+//        {
+//            var modules = new NugetyCatalog()
+//                .Options.SetModuleFileNameFilter("*Module4.dll")
+//                .FromDirectory()
+//                .GetModules<IModuleInitializer>("Module4");
 
-            var instances = modules.Load();
+//            var instances = modules.Load();
 
-            instances.FirstOrDefault().ConfigureServices(null);
-            Assert.True(!instances.OfType<InvalidInterface>().Any());
-        }
-    }
-}
+//            instances.FirstOrDefault().ConfigureServices(null);
+//            Assert.True(!instances.OfType<InvalidInterface>().Any());
+//        }
+//    }
+//}
